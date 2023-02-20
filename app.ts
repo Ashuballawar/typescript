21cbd26e76c8115d2106a9264833c06f51f6b275
Ashuballawar <ashutoshballawar@gmail.com>
2023-02-20 19:14:10 +0530

    javascript files in dist folder

--- app.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import bodyParser from "body-parser";
-import todosRoutes from './routes/todos'
-const app=express();
-app.use(bodyParser.json());
-app.use(todosRoutes)
-app.listen({port:3000});
