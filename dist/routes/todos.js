3dffa7761a5e1f141e7ded48e16596395d273640
Ashuballawar <ashutoshballawar@gmail.com>
2023-02-20 19:12:24 +0530

    separating javascript files from typescript

---
+++ dist/routes/todos.js
@@ -0,0 +1,48 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const todos = [];
+const router = (0, express_1.Router)();
+router.get('/', (req, res, next) => {
+    console.log('send success');
+    res.status(200).json({ todos: todos });
+});
+router.post('/todo', (req, res, next) => {
+    const body = req.body;
+    console.log('request');
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: body.text
+    };
+    todos.push(newTodo);
+    res.status(201).json({ msg: 'success' });
+});
+router.post('/edit', (req, res, next) => {
+    const body = req.body;
+    const tid = body.id;
+    for (let i = 0; i < todos.length; i++) {
+        if (todos[i].id === tid) {
+            todos[i].text = req.body.text;
+            return res.status(201).json({ msg: 'success' });
+        }
+    }
+    return res.status(400).json({ msg: 'not found' });
+});
+router.delete('/delete/:todoId', (req, res, next) => {
+    const params = req.params;
+    const tid = params.todoId;
+    try {
+        for (let i = 0; i < todos.length; i++) {
+            if (todos[i].id === tid) {
+                delete todos[i];
+                return res.status(200).json({ msg: 'deleted' });
+            }
+            return res.status(400).json({ msg: 'not found' });
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: 'something wrong' });
+    }
+});
+exports.default = router;
